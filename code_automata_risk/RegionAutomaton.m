a3518f94064fd477059e2658b06a1857ead2d5b1
Lindemann1989 <33152982+Lindemann1989@users.noreply.github.com>
2021-08-24 16:56:31 -0400

    Create RegionAutomaton.m

---
+++ code_automata_risk/RegionAutomaton.m
@@ -0,0 +1,37 @@
+classdef RegionAutomaton
+   properties
+       S 
+       Lambda 
+       Gamma
+       Delta
+       iota
+       lambda_e
+       gamma_e
+       lambda_s
+       gamma_s
+       b
+       c
+       F
+       p
+       reg
+       state
+       Prev
+       type
+       zeno
+       flow_time
+   end
+   methods
+       % Constructor
+       function obj = RegionAutomaton(S_,Lambda_,Gamma_,Delta_,iota_,c_,F_)
+           if nargin > 0
+               obj.S = S_;
+               obj.Lambda = Lambda_;
+               obj.Gamma = Gamma_;
+               obj.Delta = Delta_;
+               obj.iota = iota_;
+               obj.c = c_;
+               obj.F = F_;
+           end
+       end
+   end
+end
