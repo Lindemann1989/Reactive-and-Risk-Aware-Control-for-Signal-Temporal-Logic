d9df1df4bb7ba1ff0da8f5e418520f735a9e2f88
Lindemann1989 <33152982+Lindemann1989@users.noreply.github.com>
2021-08-24 16:56:19 -0400

    Create operation2.m

---
+++ code_automata_risk/operation2.m
@@ -0,0 +1,61 @@
+%Checkes transitions
+function b = operation2(lambda_e)
+    
+    b = 1;
+    % Here it's gonna be hardcoding for now, exclude states that are
+    % infeasible physically
+    
+    % First identify relevant propositions
+    reg = zeros(6,1);
+    for i=1:6
+        ind=strfind(lambda_e,['p' num2str(i)]);
+        ind_=strfind(lambda_e,['-p' num2str(i)]);
+        for j=1:length(ind)
+            if sum(ind{j})>=1 || sum(ind_{j})>=1
+                reg(i) = j;
+            end
+        end
+    end
+    
+    if reg(1)~=0 && reg(2)~=0 ...
+            && strcmp(lambda_e{reg(1)},'p1') && strcmp(lambda_e{reg(2)},'p2') 
+        % in two goal regions
+        b = 0;
+    elseif reg(1)~=0 && reg(3)~=0 ...
+            && strcmp(lambda_e{reg(1)},'p1') && strcmp(lambda_e{reg(3)},'p3') 
+        % in two goal regions
+        b = 0;
+    elseif reg(1)~=0 && reg(4)~=0 ...
+            && strcmp(lambda_e{reg(1)},'p1') && strcmp(lambda_e{reg(4)},'p4') 
+        % in two goal regions
+        b = 0;
+    elseif reg(1)~=0 && reg(6)~=0 ...
+            && strcmp(lambda_e{reg(1)},'p1') && strcmp(lambda_e{reg(6)},'p6') 
+        % in two goal regions
+        b = 0;
+    elseif reg(2)~=0 && reg(3)~=0 ...
+            && strcmp(lambda_e{reg(2)},'p2') && strcmp(lambda_e{reg(3)},'p3') 
+        % in two goal regions
+        b = 0;
+    elseif reg(2)~=0 && reg(4)~=0 ...
+            && strcmp(lambda_e{reg(2)},'p2') && strcmp(lambda_e{reg(4)},'p4') 
+        % in two goal regions
+        b = 0;
+    elseif reg(2)~=0 && reg(6)~=0 ...
+            && strcmp(lambda_e{reg(2)},'p2') && strcmp(lambda_e{reg(6)},'p6') 
+        % in two goal regions
+        b = 0;
+    elseif reg(3)~=0 && reg(4)~=0 ...
+            && strcmp(lambda_e{reg(3)},'p3') && strcmp(lambda_e{reg(4)},'p4') 
+        % in two goal regions
+        b = 0;
+    elseif reg(3)~=0 && reg(6)~=0 ...
+            && strcmp(lambda_e{reg(3)},'p3') && strcmp(lambda_e{reg(6)},'p6') 
+        % in two goal regions
+        b = 0;
+    elseif reg(4)~=0 && reg(6)~=0 ...
+            && strcmp(lambda_e{reg(4)},'p4') && strcmp(lambda_e{reg(6)},'p6') 
+        % in two goal regions
+        b = 0;
+    end
+end
