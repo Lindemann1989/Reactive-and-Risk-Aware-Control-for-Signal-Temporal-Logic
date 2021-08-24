dd194f59cb696728f58834b6d62d09e24b2cb995
Lindemann1989 <33152982+Lindemann1989@users.noreply.github.com>
2021-08-24 16:56:20 -0400

    Create operation3.m

---
+++ code_automata_risk/operation3.m
@@ -0,0 +1,24 @@
+%Checkes states
+function b = operation3(lambda_s)
+
+    % Is handled now in "Identity_uncontrollable"
+
+    b = 1;
+    
+
+end
+
+%     % Encoding of constraints for two input variables of ST2
+%     d01 : p1 a p2
+%     d02 : p1 o p2
+%     d03 : -(p1 a p2)
+%     d04 : -(p1 o p2)
+%     d05 : p1
+%     d06 : p2
+%     d07 : -p1
+%     d08 : -p2
+%     d09 : p1 a -p2
+%     d10: -p1 a p2
+%     d11: p1 o -p2
+%     d12: -p1 o p2
+%     d13: True
